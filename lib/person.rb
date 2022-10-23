90589752d79b6610e550345cb669c87669b8086b
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-10-23 17:51:35 +0300

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ require"pry"
 
 class Person
     attr_reader :name
-    attr_accessor :bank_account,  
+    attr_accessor :bank_account, :happiness, :hygiene
     def initialize (name)
         @name = name
         @bank_account = 25
